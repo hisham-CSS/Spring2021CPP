2511264c5706963cd0ebd70aff7b9184576b6a99
hisham-css <61122654+hisham-CSS@users.noreply.github.com>
2021-04-30 09:48:14 -0400

    Lab 4 Incomplete

---
+++ Assets/Scripts/Player/PlayerFire.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerFire : MonoBehaviour
+{
+    SpriteRenderer marioSprite;
+    Animator anim;
+
+    public Transform spawnPointLeft;
+    public Transform spawnPointRight;
+
+    public float projectileSpeed;
+    public Projectile projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        marioSprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        if (projectileSpeed <= 0)
+            projectileSpeed = 7.0f;
+
+        if (!spawnPointLeft || !spawnPointRight || !projectilePrefab)
+            Debug.Log("Unity Inspector Values Not Set");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetBool("isShooting", true);
+        }
+    }
+
+    void FireProjectile()
+    {
+        if (marioSprite.flipX)
+        {
+            Projectile projectileInstance = Instantiate(projectilePrefab, spawnPointLeft.position, spawnPointLeft.rotation);
+            projectileInstance.speed = projectileSpeed;
+        }
+        else
+        {
+            Projectile projectileInstance = Instantiate(projectilePrefab, spawnPointRight.position, spawnPointRight.rotation);
+            projectileInstance.speed = projectileSpeed;
+        }
+    }
+
+    void ResetFire()
+    {
+        anim.SetBool("isShooting", false);
+    }
+}
